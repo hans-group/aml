88eac25d6453fe21840414b4a4f5358c959e8816
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-08-10 16:55:05 +0900

    expose opt to simulations

--- aml/simulations/__init__.py
+++ aml/simulations/__init__.py
@@ -1,9 +1,12 @@
 from .ase_interface import AMLCalculator
 from .md import MolecularDynamics
+from .optimization import BatchedGeometryOptimization, GeometryOptimization
 from .vibration import NormalModes
 
 __all__ = [
     "AMLCalculator",
+    "BatchedGeometryOptimization",
+    "GeometryOptimization",
     "MolecularDynamics",
     "NormalModes",
 ]
