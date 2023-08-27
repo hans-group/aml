b79615895b2112de9512ec979b698cf4172310c5
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-08-27 12:23:01 +0900

    add default dtype as top-level attr

--- aml/__init__.py
+++ aml/__init__.py
@@ -1,5 +1,9 @@
+import torch
+
 from . import common, data, models, nn, simulations, train, typing
 from .api import compile_iap, load_iap, load_pretrained_model
+
+_default_float = torch.get_default_dtype()
 
 __all__ = [
     "simulations",
