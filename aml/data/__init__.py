e2829e8b34e8cf637969159bd13205eff7ac94d5
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-08-28 15:49:49 +0900

    add LMDBDataset to init

--- aml/data/__init__.py
+++ aml/data/__init__.py
@@ -1,11 +1,12 @@
 from . import keys, neighbor_list
 from .data_structure import AtomsGraph
-from .dataset import ASEDataset, SimpleDataset
+from .dataset import ASEDataset, LMDBDataset, SimpleDataset
 
 __all__ = [
     "AtomsGraph",
     "SimpleDataset",
     "ASEDataset",
+    "LMDBDataset",
     "keys",
     "neighbor_list",
 ]
