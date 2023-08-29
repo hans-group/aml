afd1b60e689594a764c9b63acd8eb3acae401f8d
Minjoon Hong <mjhong0708@naver.com>
2023-08-29 16:13:54 +0900

    add inmemory dataset import

--- aml/data/dataset/__init__.py
+++ aml/data/dataset/__init__.py
@@ -1,9 +1,10 @@
 from .ase import ASEDataset
-from .base import BaseDataset
+from .base import BaseDataset, InMemoryDataset
 from .lmdb import LMDBDataset
 
 __all__ = [
     "BaseDataset",
+    "InMemoryDataset",
     "LMDBDataset",
     "ASEDataset",
 ]
