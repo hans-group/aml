b79b27dd2b16bdf6fb2297fc7ceba746ba972c93
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-09-11 21:30:39 +0900

    Added docstring to utils.py

--- aml/common/utils.py
+++ aml/common/utils.py
@@ -1,3 +1,5 @@
+"""This module contains utility functions and classes.
+"""
 import functools
 import inspect
 import json
@@ -7,6 +9,7 @@ from copy import deepcopy
 from numbers import Number
 from os import PathLike
 from pathlib import Path
+from typing import Any
 
 import ase.data
 import numpy as np
@@ -16,7 +19,15 @@ from aml.data import keys as K
 from aml.typing import DataDict, Tensor
 
 
-def _get_init_args(cls):
+def _get_init_args(cls) -> tuple[list[str], list[Any]]:
+    """Get the arguments of the __init__ function of a class.
+
+    Args:
+        cls: The class to get the arguments from.
+
+    Returns:
+        tuple[list[str], list[Any]]: The arguments and their default values.
+    """
     params = inspect.signature(cls.__init__).parameters
     args = list(params.keys())[1:]
     defaults = [params[arg].default for arg in args]
@@ -24,9 +35,27 @@ def _get_init_args(cls):
 
 
 class Configurable:
+    """Mixin class for configurable classes.
+    Any class that inherits from this class can be initialized from a config dict (``from_config``),
+    and can be converted to a config dict(``get_config``).
+    Supports nested Configurable objects, and any object that has an ``as_dict`` method.
+    """
+
     supported_val_types = (str, bool, Number, type(None))
 
-    def get_config(self, param_name_map=None):
+    def get_config(self, param_name_map: dict[str, str] | None = None) -> dict[str, Any]:
+        """Get the config of the object.
+        The object must contain the attributes that are used in the __init__ function.
+        If the name of attribute does not match to the name of the parameter in the __init__ function,
+        use ``param_name_map`` to map the attribute name to the parameter name.
+        Any attributes that is instance of ``Configurable`` will be converted to config dict recursively.
+
+        Args:
+            param_name_map (dict[str, str], optional): A mapping from the parameter names to the config names.
+
+        Returns:
+            dict[str, Any]: The config dict.
+        """
         param_name_map = param_name_map or {}
         args, _ = _get_init_args(self.__class__)
 
@@ -51,13 +80,30 @@ class Configurable:
             try:
                 val = getattr(self, argname)
             except AttributeError as e:
-                raise ValueError(f"Argument {arg} should be present as attribute.") from e
+                raise ValueError(
+                    f"Argument {arg}(mapped name: {param_name_map.get(arg, arg)}) "
+                    "should be present as attribute of the object."
+                ) from e
             config[arg] = recursive_as_dict(val)
 
         return config
 
     @classmethod
-    def from_config(cls, config, actual_cls=None):
+    def from_config(cls, config: dict[str, Any], actual_cls: type | None = None) -> Any:
+        """Initialize the object from a config dict.
+        In case that the object which calls ``from_config`` is different from the object to be initialized,
+        use ``actual_cls`` to specify the class to be initialized.
+        This is useful when many classes share one base class, so that the base class can be used to initialize.
+        ex) ``BaseDataset.from_config(config, actual_cls=MyDataset)``
+
+        Args:
+            config (dict[str, Any]): The config dict.
+            actual_cls (type, optional): The class to be initialized. Defaults to None.
+
+        Returns:
+            Any: The initialized object.
+        """
+
         if actual_cls is None:
             actual_cls = cls
         config = deepcopy(config)
@@ -82,7 +128,7 @@ class Configurable:
 
 
 def warn_unstable(cls_or_fn):
-    """Warns that a class or function is unstable.
+    """Decorator that warns that a class or function is unstable.
 
     Args:
         cls_or_fn: Class or function to warn about.
@@ -126,13 +172,33 @@ def log_and_print(contents: str, filepath: PathLike = None, end="\n"):
     print(contents, end=end)
 
 
-def remove_unused_kwargs(func, kwargs):
-    """Remove unused kwargs from a function call."""
+def remove_unused_kwargs(func: callable, kwargs: dict[str, Any]) -> dict[str, Any]:
+    """Remove unused kwargs from a function call.
+
+    Args:
+        func (callable): The function to call.
+        kwargs (dict[str, Any]): The kwargs to pass to the function.
+
+    Returns:
+        dict[str, Any]: The kwargs that are used in the function.
+    """
     valid_args = inspect.signature(func).parameters
     return {k: v for k, v in kwargs.items() if k in valid_args}
 
 
 def compute_neighbor_vecs(data: DataDict) -> DataDict:
+    """Compute the vectors between atoms and their neighbors (i->j)
+    and store them in ``data[K.edge_vec]``.
+    The ``data`` must contain ``data[K.pos]``, ``data[K.edge_index]``, ``data[K.edge_shift]``,
+    This function should be called inside ``forward`` since the dependency of neighbor positions
+    on atomic positions needs to be tracked by autograd in order to appropriately compute forces.
+
+    Args:
+        data (DataDict): The data dictionary.
+
+    Returns:
+        DataDict: The data dictionary with ``data[K.edge_vec]``.
+    """
     batch = data[K.batch]
     pos = data[K.pos]
     edge_index = data[K.edge_index]  # neighbors
@@ -149,6 +215,18 @@ def compute_neighbor_vecs(data: DataDict) -> DataDict:
 
 
 def canocialize_species(species: Tensor | list[int] | list[str]) -> Tensor:
+    """Convert species to a tensor of atomic numbers.
+
+    Args:
+        species (Tensor | list[int] | list[str]): The species.
+            One of the following:
+                - A tensor of atomic numbers.
+                - A list of atomic numbers.
+                - A list of atomic symbols.
+
+    Returns:
+        Tensor: The species as a tensor of atomic numbers.
+    """
     if isinstance(species[0], str):
         species = [ase.data.atomic_numbers[s] for s in species]
         species = torch.as_tensor(species, dtype=torch.long)
@@ -160,11 +238,22 @@ def canocialize_species(species: Tensor | list[int] | list[str]) -> Tensor:
 
 
 def get_batch_size(batch: DataDict) -> int:
-    return batch[K.batch][-1] + 1
+    """Get the batch size of a data batch.
+
+    Args:
+        batch (DataDict): The data batch.
+
+    Returns:
+        int: The batch size.
+    """
+    if K.batch in batch:
+        return batch[K.batch][-1] + 1
+    return 1
 
 
 def load_config(filepath: PathLike) -> dict:
-    """Load a config file.
+    """A convinience function to load a config file using the correct loader.
+    supports json, yaml, and toml.
 
     Args:
         filepath (PathLike): The path to the config file.
