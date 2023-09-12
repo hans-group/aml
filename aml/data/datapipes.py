990439a54777b37d127b2826f2ae87bdeecdbe73
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-09-12 10:19:12 +0900

    Add docstrings to datapipes.py

--- aml/data/datapipes.py
+++ aml/data/datapipes.py
@@ -1,6 +1,6 @@
 import os
 from collections.abc import Sequence
-from typing import Dict
+from typing import Dict, Literal
 
 import ase.data
 import ase.io
@@ -14,7 +14,17 @@ from .data_structure import AtomsGraph
 
 @functional_datapipe("read_ase")
 class ASEFileReader(IterDataPipe):
-    def __init__(self, dp):
+    """
+    Read atoms from ase readable file.
+    Multiple files can be read by providing a list of filenames.
+
+    Args:
+        dp (IterDataPipe): Iterable data pipe that yields str or (str, str) where the first str is the filename and the
+            second str is the index of the atoms in the file. If the second str is not provided,
+            all atoms in the file will be read.
+    """
+
+    def __init__(self, dp: IterDataPipe):
         self.dp = dp
 
     def __iter__(self):
@@ -33,9 +43,18 @@ class ASEFileReader(IterDataPipe):
 
 @functional_datapipe("atoms_to_graph")
 class AtomsGraphParser(IterDataPipe):
+    """
+    Convert ase.Atoms to AtomsGraph.
+
+    Args:
+        dp (IterDataPipe): Iterable data pipe that yields ase.Atoms.
+        read_properties (bool): Whether to read properties (energy, ...) from ase.Atoms. Default: True.
+
+    """
+
     default_float_dtype = torch.get_default_dtype()
 
-    def __init__(self, dp, read_properties=True):
+    def __init__(self, dp: IterDataPipe, read_properties: bool = True):
         self.dp = dp
         self.read_properties = read_properties
 
@@ -49,7 +68,23 @@ class AtomsGraphParser(IterDataPipe):
 
 @functional_datapipe("build_neighbor_list")
 class NeighborListBuilder(IterDataPipe):
-    def __init__(self, dp, cutoff, self_interaction=False, backend="ase", **kwargs):
+    """Neighbor list builder.
+
+    Args:
+        dp (IterDataPipe): Iterable data pipe that yields AtomsGraph.
+        cutoff (float): Cutoff radius for neighbor list.
+        self_interaction (bool): Whether to include self interaction. Default: False.
+        backend (str): Backend for neighbor list builder. Default: "ase".
+    """
+
+    def __init__(
+        self,
+        dp: IterDataPipe,
+        cutoff: float,
+        self_interaction: bool = False,
+        backend: Literal["ase", "torch", "matscipy"] = "ase",
+        **kwargs,
+    ):
         self.dp = dp
         self.cutoff = cutoff
         self.self_interaction = self_interaction
@@ -71,7 +106,16 @@ class NeighborListBuilder(IterDataPipe):
 
 @functional_datapipe("standardize_property")
 class PropertyStandardizer(IterDataPipe):
-    def __init__(self, dp, mean, std, target):
+    def __init__(self, dp: IterDataPipe, mean: float, std: float, target: str):
+        """Standardize property in data.
+
+        Args:
+            dp (IterDataPipe): Iterable data pipe that yields torch_geometric.data.Data.
+            mean (float): Mean of the property.
+            std (float): Standard deviation of the property.
+            target (str): Property to be standardized.
+
+        """
         self.dp = dp
         self.mean = mean
         self.std = std
@@ -89,7 +133,16 @@ class PropertyStandardizer(IterDataPipe):
 
 @functional_datapipe("unstandardize_property")
 class PropertyUnStandardizer(IterDataPipe):
-    def __init__(self, dp, mean, std, target):
+    def __init__(self, dp: IterDataPipe, mean: float, std: float, target: str):
+        """Unstandardize property in data.
+
+        Args:
+            dp (IterDataPipe): Iterable data pipe that yields torch_geometric.data.Data.
+            mean (float): Mean of the property.
+            std (float): Standard deviation of the property.
+            target (str): Property to be unstandardized.
+
+        """
         self.dp = dp
         self.mean = mean
         self.std = std
@@ -107,7 +160,14 @@ class PropertyUnStandardizer(IterDataPipe):
 
 @functional_datapipe("subtract_atomref")
 class SubtractAtomref(IterDataPipe):
-    def __init__(self, dp, atomic_energies: Dict[str, float]):
+    """Subtract atomic energy from total energy.
+
+    Args:
+        dp (IterDataPipe): Iterable data pipe that yields torch_geometric.data.Data.
+        atomic_energies (Dict[str, float]): Atomic energies.
+    """
+
+    def __init__(self, dp: IterDataPipe, atomic_energies: Dict[str, float]):
         self.dp = dp
         self.atomic_energies = atomic_energies
 
