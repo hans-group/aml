d460476789b42cac870be0504c3ed27fa38f7560
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-09-12 10:29:43 +0900

    Added docstring to utils.py

--- aml/data/utils.py
+++ aml/data/utils.py
@@ -1,14 +1,23 @@
 import pickle
 import zlib
-from typing import TypeVar
+from typing import Iterable, TypeVar
 
 import lmdb
+from torch_geometric.data import Data
 
 T = TypeVar("T")
-T_co = TypeVar("T_co", covariant=True)
 
 
 def maybe_list(x: T | list[T]) -> list[T]:
+    """Listify x if it is not a list.
+    TODO: Rewrite to use `typing.Sequence` instead of `list`.
+
+    Args:
+        x (T | list[T]): The input.
+
+    Returns:
+        list[T]: The listified input.
+    """
     if isinstance(x, list):
         return x
     else:
@@ -16,6 +25,14 @@ def maybe_list(x: T | list[T]) -> list[T]:
 
 
 def is_pkl(b: bytes) -> bool:
+    """Check if the bytes is a pickle file by checking the first two bytes.
+
+    Args:
+        b (bytes): The bytes.
+
+    Returns:
+        bool: Whether the bytes is a pickle file.
+    """
     first_two_bytes = b[:2]
     if first_two_bytes in (b"cc", b"\x80\x02", b"\x80\x03", b"\x80\x04", b"\x80\x05"):
         return True
@@ -23,7 +40,7 @@ def is_pkl(b: bytes) -> bool:
 
 
 def write_lmdb_dataset(
-    data_iter,
+    data_iter: Iterable[Data],
     path: str,
     map_size: int = 1099511627776 * 2,
     compress: bool = False,
@@ -33,8 +50,10 @@ def write_lmdb_dataset(
 
     Args:
         data_iter (Iterable): Iterable of data.
+        path (str): Path to the database.
         map_size (int, optional): Size of memory mapping. Defaults to 1099511627776*2.
         compress (bool, optional): Whether to compress data by zlib. Defaults to False.
+        metadata (dict, optional): Metadata to store in the database. Defaults to None.
     """
     db = lmdb.open(path, map_size=map_size, subdir=False, meminit=False, map_async=True)
 
