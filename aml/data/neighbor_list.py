aff8472d1e9601bca58845c9bf1db4275bb6f851
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-09-12 10:27:03 +0900

    Add docstring to neighbor_list.py

--- aml/data/neighbor_list.py
+++ aml/data/neighbor_list.py
@@ -12,13 +12,31 @@ from torch_geometric.data import Data
 
 
 def distance_to_face(x: np.ndarray, face_vec_1: np.ndarray, face_vec_2: np.ndarray) -> float:
+    """Compute the distance between a point and a face of a parallelepiped.
+
+    Args:
+        x: The point.
+        face_vec_1: The first vector of the face.
+        face_vec_2: The second vector of the face.
+
+    Returns:
+        The distance between the point and the face.
+    """
     face_normal_vec = np.cross(face_vec_1, face_vec_2)
     face_normal_vec /= np.linalg.norm(face_normal_vec)
     return np.abs(np.dot(face_normal_vec, x))
 
 
 def minimum_distance_to_cell(x: Tensor, cell: Tensor) -> float:
-    """Compute the minimum distance between a point and a cell."""
+    """Compute the minimum distance between a point and a cell.
+
+    Args:
+        x: The point.
+        cell: The cell.
+
+    Returns:
+        The minimum distance between the point and the cell.
+    """
     vec_a = cell[0]
     vec_b = cell[1]
     vec_c = cell[2]
@@ -30,12 +48,28 @@ def minimum_distance_to_cell(x: Tensor, cell: Tensor) -> float:
 
 
 class NeighborList(NamedTuple):
+    """The namedtuple that stores neighbor list.
+
+    Attributes:
+        center_idx: The indices of center atoms.
+        neighbor_idx: The indices of neighbor atoms.
+        offset: The offset of neighbor atoms.
+    """
+
     center_idx: Tensor
     neighbor_idx: Tensor
     offset: Tensor
 
 
 class NeighborListBuilder(ABC):
+    """Base class for neighbor list builder.
+
+    Args:
+        cutoff (float): Cutoff radius for neighbor list.
+        self_interaction (bool): Whether to include self interaction. Default: False.
+
+    """
+
     def __init__(self, cutoff: float, self_interaction: bool = False):
         self.cutoff = cutoff
         self.self_interaction = self_interaction
@@ -46,6 +80,15 @@ class NeighborListBuilder(ABC):
 
 
 class ASENeighborListBuilder(NeighborListBuilder):
+    """ASE neighbor list builder.
+    Usually the slowest one, but only one that supports large cutoff for small unit cell.
+
+    Args:
+        cutoff (float): Cutoff radius for neighbor list.
+        self_interaction (bool): Whether to include self interaction. Default: False.
+
+    """
+
     def build(self, atoms_graph: Data) -> NeighborList:
         if atoms_graph.volume() == 0:
             pbc = np.array([False, False, False])
@@ -67,6 +110,17 @@ class ASENeighborListBuilder(NeighborListBuilder):
 
 
 class MatscipyNeighborListBuilder(NeighborListBuilder):
+    """Matscipy neighbor list builder.
+    Fast for both periodic and non-periodic systems.
+    Uses minimum image convention (MIC) for periodic systems, so
+    the neighbor list may be wrong if the cutoff is larger than the cell.
+
+    Args:
+        cutoff (float): Cutoff radius for neighbor list.
+        self_interaction (bool): Whether to include self interaction. Default: False.
+
+    """
+
     def build(self, atoms_graph: Data) -> NeighborList:
         if atoms_graph.volume() == 0:
             pbc = np.array([False, False, False])
@@ -114,6 +168,17 @@ class MatscipyNeighborListBuilder(NeighborListBuilder):
 
 
 class TorchNeighborListBuilder(NeighborListBuilder):
+    """Neighbor list builder using ``radius_graph`` from ``torch_cluster``.
+    Only this one supports GPU. The fastest one for non-periodic systems.
+    For periodic system, it makes supercell and uses ``radius_graph``, so the
+    performance is comparable to ``matscipy``.
+
+    Args:
+        cutoff (float): Cutoff radius for neighbor list.
+        self_interaction (bool): Whether to include self interaction. Default: False.
+
+    """
+
     def build(self, atoms_graph: Data) -> NeighborList:
         max_neighbors = 64
         device = atoms_graph.pos.device
@@ -152,7 +217,15 @@ _neighborlistbuilder_cls_map = {
 }
 
 
-def resolve_neighborlist_builder(neighborlist_backend: Union[str, object]) -> object:
+def resolve_neighborlist_builder(neighborlist_backend: Union[str, object]) -> type:
+    """Resolve neighbor list builder by backend name.
+
+    Args:
+        neighborlist_backend (str or object): The backend name or the backend class.
+
+    Returns:
+        The backend class.
+    """
     if isinstance(neighborlist_backend, str):
         return _neighborlistbuilder_cls_map[neighborlist_backend]
     return neighborlist_backend
