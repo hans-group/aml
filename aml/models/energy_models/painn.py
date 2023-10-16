eeb81b04977a7cfd7976cd69ae1d77da7ade5961
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2023-10-16 16:45:23 +0900

    Use internal `scatter` in painn

--- aml/models/energy_models/painn.py
+++ aml/models/energy_models/painn.py
@@ -1,8 +1,7 @@
 from typing import Literal
 
-from torch_geometric.utils import scatter
-
 from aml.common.registry import registry
+from aml.common.scatter import scatter
 from aml.common.utils import compute_neighbor_vecs
 from aml.data import keys as K
 from aml.nn.mlp import MLP
